f16ae30c919163709ead9b0b3c7f119b4ee82b9c
zavialovserge <zavialovserge@gmail.com>
2016-02-16 23:26:42 +0200

    Laboratory_1_1_1

---
+++ Laboratory1_1_1/LAb1/Laboratory1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAb1
+{
+    class Laboratory1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            ////Console.WriteLine("Hello world");
+        }
+    }
+}
