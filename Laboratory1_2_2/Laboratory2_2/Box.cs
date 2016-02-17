93b53d73ae10263124021420a982b1754a31506a
zavialovserge <zavialovserge@gmail.com>
2016-02-17 14:49:44 +0200

    Laboratory_1_2_2

---
+++ Laboratory1_2_2/Laboratory2_2/Box.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory2_2
+{
+    class Box
+    {
+        private int setX{get;set;}
+        private int setY { get; set; }
+        private int setWidth { get; set; }
+        private int setHeight { get; set; }
+        private char setSymbol { get; set; }
+        private string message { get; set; }
+        public Box()
+        { 
+        
+        }
+        public Box(int _setX, int _setY, int _setWidth, int _setHeight, char _setSymbol,ref string _message)
+        {
+            setX = _setX;
+            setY = _setY;
+            setWidth = _setWidth;
+            setHeight = _setHeight;
+            setSymbol = _setSymbol;
+            message = _message;
+
+        }
+        public void Draw()
+        {
+            if (setHeight > 0 || setWidth > 0 || setX > 0 || setY > 0)
+            {
+                for (int i = 0; i < setHeight; i++)
+                {
+                       for (int j = 0; j < setWidth; j++)
+                            {
+                               Console.SetCursorPosition((j + setX), (i + setY));
+                                if (i == 0 || i == (setHeight-1))
+                                {
+                                    Console.Write(setSymbol);
+                                }
+                                else if (i >= 1 && i < (setHeight-1))
+                                {
+                                    if (j == 0 || j == (setWidth-1))
+                                    {
+                                        Console.Write(setSymbol);
+                                    }
+                                    else
+                                    {
+                                        if ((setWidth >= 3) || (setHeight >= 3))
+                                        {
+                                            Console.SetCursorPosition(setX + setWidth / 2, setY + setHeight / 2);
+                                            if (message == null)
+                                            {
+                                                message = "";
+                                            }
+                                            else if (message.Length < setWidth)
+                                            {
+                                                Console.Write(message);
+                                            }
+                                            else if (message.Length >= setWidth)
+                                            {
+                                                string mess;
+                                                mess = message.Substring(0, setWidth - 3);
+                                                Console.Write(mess);
+                                            }
+
+                                        }
+                                    }
+                                    }
+                                    else
+                                    {
+                                        Console.Write(" ");
+                                    }
+                                }
+                            }
+                        Console.WriteLine();
+            }
+            else if (setHeight == 0 || setWidth == 0)
+            {
+                Console.WriteLine("Height or width = 0, Please reload programmand enter cuurect values!!!");
+            }
+            else
+            {
+                Console.WriteLine("Height or width < 0, Please reload programmand enter cuurect values!!!!");
+            }
+        }
+    }
+}
---
+++ Laboratory1_2_2/Laboratory2_2/Laboratory2_2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory2_2
+{
+    class Laboratory2_2
+    {
+        static void Main(string[] args)
+        {
+           
+           Console.WriteLine("Please enter first position(x):");
+           int x = int.Parse(Console.ReadLine());
+
+           Console.WriteLine("Please enter first position(y):");
+           int y = int.Parse(Console.ReadLine());
+
+           Console.WriteLine("Please enter width :");
+           int width = int.Parse(Console.ReadLine());
+
+           Console.WriteLine("Please enter height:");
+           int height = int.Parse(Console.ReadLine());
+
+           Console.WriteLine("Please enter drawing symbol(*,+,.):");
+           char symb = char.Parse(Console.ReadLine());
+
+           Console.WriteLine("Please enter message:");
+           string mess = Console.ReadLine();
+
+           Box box = new Box(x,y,width,height,symb,ref mess);
+           Console.Clear();
+           box.Draw();
+            //delay
+           Console.ReadKey();
+        }
+    }
+}
